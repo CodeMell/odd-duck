0385010518f702c0993c1d233daaf941f0eb79c8
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-03-21 11:56:20 -0500

    update

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ const state = {
     if (clicks === maxClicksAllowed) {
       productContainer.removeEventListener('click', handleProductClick);
       renderResults();
-      productContainer.className = 'no-voting';
+      // productContainer.className = 'no-voting';
     } else {
       renderProducts();
     }
